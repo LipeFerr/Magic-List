44b3dc0eb8cad8bbd82b94b97bfc0154c0659ab7
Felipe Ferreira <108698714+LipeFerr@users.noreply.github.com>
2024-01-05 19:05:15 -0300

    Update CardListCardService.js

--- assets/js/CardListCardService.js
+++ assets/js/CardListCardService.js
@@ -23,7 +23,7 @@ export function generateList(id) {
   }
 
   export function insertCardListCard(CardListCard) {
-    const endpointURL = 'http://localhost:8080/CardListCard';
+    const endpointURL = 'http://44.203.181.82:8080/CardListCard';
     //console.log(CardListCard);
     const requestOptions = {
       method: 'POST',
